2c5c985748b4347aa895622a6c1c30395aa28e09
juanmavelezpa <juanmavelezpa@gmail.com>
2022-09-20 10:31:09 -0500

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,13 +34,23 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseHttpsRedirection();
             app.UseRouting();
+            app.UseCors((x) =>
+            {
+                x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+            });
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
